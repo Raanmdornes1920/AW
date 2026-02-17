7cbae99c02a2c7f0558a656621cb6b48468206d6
Raanmdornes1920 <rsalaz01@ucm.es>
2026-02-17 18:14:55 +0100

    Cambios

---
+++ practica2/index.php
@@ -0,0 +1,9 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<html><head><title>PHP</title></head><body>;
+echo '<h1>Prueba PHP</h1>';
+for ($i = 0; $i < 50; $i++) {
+echo '<p>' . $i . ' - ¡Hola Mundo!</p>';
+}
+echo '</body></html>';
+?>
