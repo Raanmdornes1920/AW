d3f6af2f2db8f802788a5b7985dd17703ac06bb6
AngelaMeiras <148640547+AngelaMeiras@users.noreply.github.com>
2026-03-13 10:25:10 +0100

    Hecho: Cambiar a por button y href por onclick

--- practica2/includes/vistas/usuarios/inicio.php
+++ practica2/includes/vistas/usuarios/inicio.php
@@ -11,8 +11,8 @@
 
             echo '<div class="contenedor-botones-iniciar-pedido">';
             // TODO: Cambiar a por button y href por onclick
-                echo '<a href="' . RAIZ_APP . '/includes/vistas/categorias/categorias_cliente.php?tipo=llevar" class="boton-iniciar-pedido">Para llevar</a>';
-                echo '<a href="' . RAIZ_APP . '/includes/vistas/categorias/categorias_cliente.php?tipo=local" class="boton-iniciar-pedido">Para consumir en el local</a>';
+                echo '<button onclick="window.location.href=\'' . RAIZ_APP . '/includes/vistas/categorias/categorias_cliente.php?tipo=llevar\'" class="boton-iniciar-pedido">Para llevar</button>';
+                echo '<button onclick="window.location.href=\'' . RAIZ_APP . '/includes/vistas/categorias/categorias_cliente.php?tipo=local\'" class="boton-iniciar-pedido">Para consumir en el local</button>';
             echo '</div>';
         echo '</div>';
     } elseif($_SESSION['usuario']->rol() ==='camarero') {
