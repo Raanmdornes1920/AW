0a566690c603c16436c1a250b458924d72d0a26d
Raanmdornes1920 <rsalaz01@ucm.es>
2026-04-10 00:11:08 +0200

    editar avatar

--- practica3/includes/vistas/usuarios/apoyo/editar_avatar.php
+++ practica3/includes/vistas/usuarios/apoyo/editar_avatar.php
@@ -1,5 +1,5 @@
 <?php
-require_once (__DIR__ . '/../../../config.php');
+require_once(__DIR__ . '/../../../config.php');
 session_start();
 $SA = new UsuarioSA($db_connection);
 
@@ -9,40 +9,38 @@ if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
+
     // Verificamos si seleccionó alguna imagen default
-    if (isset($_POST['foto_perfil']) && $_POST['foto_perfil'] !== 'custom'){
+    if (isset($_POST['foto_perfil']) && $_POST['foto_perfil'] !== 'custom') {
         $rutaAlArchivo = "../img/perfiles/" . $_SESSION['usuario']->avatar();
         $nombreImagen = mysqli_real_escape_string($db_connection, $_POST['foto_perfil']);
-        
+
         if (!$SA->modificarUsuario($_SESSION['usuario']->id(), 'avatar', $nombreImagen)) {
             $_SESSION['error_editar_perfil'] = "Error al cambiar la imagen.";
             header("Location: " . RUTA_VISTAS . "/editar_perfil.php");
             exit();
-        }
-        else{
+        } else {
             $_SESSION['usuario']->set_avatar($nombreImagen);
         }
-        
-        
+
+
         if (!in_array($_SESSION['usuario']->avatar(), IMAGENES_BASE) && file_exists($rutaAlArchivo)) {
             unlink($rutaAlArchivo);
         }
-
     }
     // Si no eligio una default la procesamos
     elseif (isset($_FILES['foto_perfil']) && $_FILES['foto_perfil']['error'] === UPLOAD_ERR_OK) {
-        
+
         $rutaAlArchivo = "../img/perfiles/" . $_SESSION['usuario']->avatar();
 
         $fileTmpPath = $_FILES['foto_perfil']['tmp_name'];
         $fileName = $_FILES['foto_perfil']['name'];
-        
+
         $fileNameClean = time() . "_" . preg_replace("/[^a-zA-Z0-9.]/", "_", $fileName);
-        
+
         $dest_path = "../img/perfiles/" . $fileNameClean;
-        
-        if(move_uploaded_file($fileTmpPath, $dest_path)) {
+
+        if (move_uploaded_file($fileTmpPath, $dest_path)) {
             $nombreImagen = $fileNameClean;
             chmod($dest_path, 0666);
 
@@ -50,22 +48,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $_SESSION['error_editar_perfil'] = "Error al subir la imagen.";
                 header("Location: " . RUTA_VISTAS . "/editar_perfil.php");
                 exit();
-            }
-            else{
+            } else {
                 $_SESSION['usuario']->set_avatar($nombreImagen);
             }
-            
+
             if (file_exists($rutaAlArchivo)) {
                 unlink($rutaAlArchivo);
             }
-        }
-        else {
+        } else {
             $_SESSION['error_editar_perfil'] = "Error al subir la imagen.";
             header("Location: " . RUTA_VISTAS . "/editar_perfil.php");
             exit();
         }
-    }
-    else {
+    } else {
         $_SESSION['error_editar_perfil'] = "Error al subir la imagen.";
         header("Location: " . RUTA_VISTAS . "/editar_perfil.php");
         exit();
@@ -77,4 +72,3 @@ $_SESSION['cambio'] = "Avatar";
 $_SESSION['error_editar_perfil'] = "Ninguno";
 header("Location: " . RUTA_VISTAS . "/usuarios/editar_perfil.php");
 exit();
-?>
